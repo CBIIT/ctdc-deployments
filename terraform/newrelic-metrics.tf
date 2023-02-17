2bd7a8de0f96fc0c85f39eb906f313c81ebdc32f
Michael Fleming <michael.fleming@nih.gov>
2023-02-17 15:39:39 -0500

    added conditional

--- terraform/newrelic-metrics.tf
+++ terraform/newrelic-metrics.tf
@@ -29,8 +29,8 @@ variable "program" {
 
 # modules
 module "new_relic_metric_pipeline" {
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/firehose-metrics?ref=v1.0"
-
+  count                    = var.create_newrelic_pipeline ? 1 : 0
+  source                   = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/firehose-metrics?ref=v1.0"
   account_id               = data.aws_caller_identity.current.account_id
   app                      = var.stack_name
   http_endpoint_access_key = var.newrelic_api_key
