4e3b185fde45edb555bb23da4367a84776de219d
Michael Fleming <michael.fleming@nih.gov>
2023-03-03 16:19:22 -0500

    updated to not use permissions boundary for upper tiers

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,7 +9,7 @@ locals {
   neo4j_https                     = 7473
   neo4j_bolt                      = 7687
   integration_server_profile_name = "${var.iam_prefix}-integration-server-profile"
-  permissions_boundary            = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PermissionBoundary_PowerUser"
+  permissions_boundary            = terraform.workspace == "dev" || terraform.workspace == "qa" ? "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PermissionBoundary_PowerUser" : null
   #nih_ip_cidrs =  terraform.workspace == "prod" || terraform.workspace == "stage" ? ["0.0.0.0/0"] : [ "129.43.0.0/16" , "137.187.0.0/16"  , "165.112.0.0/16" , "156.40.0.0/16"  , "128.231.0.0/16" , "130.14.0.0/16" , "157.98.0.0/16"]
   nih_ip_cidrs = ["0.0.0.0/0"]
   all_ips      =  local.nih_ip_cidrs
