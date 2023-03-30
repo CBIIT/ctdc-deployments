c27ab26b85dc3eff663596d498461faf61a44855
Fleming <flemingme@nih.gov>
2023-03-30 16:06:15 -0400

    updated module version

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,6 @@
 module "ecr" {
   count                    = var.create_ecr_repos ? 1 : 0
-  source                   = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/ecr?ref=v1.0"
+  source                   = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/ecr?ref=v1.2"
   project                  = var.project
   env                      = terraform.workspace
   resource_prefix          = local.resource_prefix
