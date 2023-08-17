e1f8c7a732b7f6c78ec3bdbf6179172205024920
Kotepalli <kotepalliv2@nih.gov>
2023-08-17 16:58:03 -0400

    corrected the inputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,6 @@ module "alb" {
   source              = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/loadbalancer?ref=v1.8"
   resource_prefix     = "${var.program}-${terraform.workspace}-${var.project}"
   vpc_id              = var.vpc_id
-  alb_log_bucket_name = module.s3.bucket_name
   env                 = terraform.workspace
   alb_internal        = var.internal_alb
   alb_type            = var.lb_type
@@ -11,6 +10,7 @@ module "alb" {
   alb_subnet_ids      = var.private_subnet_ids
   tags                = var.tags
   stack_name          = var.project
+  program             = "crdc"
   alb_certificate_arn = data.aws_acm_certificate.amazon_issued.arn
 }
 
