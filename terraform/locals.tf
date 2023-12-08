aecb3ce99a088e54cfc25e62733cdc9dcdaa6900
Kotepalli <kotepalliv2@nih.gov>
2023-12-08 09:25:06 -0500

    added missing S3 bucket name in locals

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -36,6 +36,8 @@ locals {
   # ECS
   application_url = terraform.workspace == "prod" ? var.domain_name : "${var.application_subdomain}-${terraform.workspace}.${var.domain_name}"
 
+  s3_snapshot_bucket_name = "${var.program}-${terraform.workspace}-${var.project}-opensearch-snapshot-bucket"
+
   # Secrets
   dynamic_secrets = {
     app = {
