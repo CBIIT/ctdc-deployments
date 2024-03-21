0385dd768fac0ac7a9b95db7f8ac77badf74eb99
Kotepalli <kotepalliv2@nih.gov>
2024-03-21 18:13:00 -0400

    updating the tag values

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,4 +9,19 @@ terraform {
 
 provider "aws" {
   region = var.region
+  
+  default_tags {
+    tags = {
+      Customer       = "nci od cbiit ods"
+      DevLead        = "yizhen chen"
+      DevOps         = "venkatasaikiran kotepalli"
+      FISMA          = "moderate"
+      ManagedBy      = "terraform"
+      OpsModel       = "cbiit managed hybrid"
+      Program        = "crdc"
+      PII            = "yes"
+      Project        = "ctdc"
+      ProjectManager = "Hayley Dingerdissen"
+    }
+  }
 }
