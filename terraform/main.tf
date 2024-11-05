17c67c5a3899d23f53a11f8a40315cd5ffa18d9d
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-11-05 17:03:36 -0500

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,26 +80,18 @@ module "new_relic_metric_pipeline" {
   resource_prefix          = "${var.program}-${var.project}-${var.account_level}"
 }
 
-# Opensearch
 module "opensearch" {
-  count = var.create_opensearch_cluster ? 1: 0
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/opensearch?ref=v1.19"
-  resource_prefix     = "${var.stack_name}-${terraform.workspace}"
-  stack_name = var.stack_name
-  tags = var.tags
-  opensearch_instance_type = var.opensearch_instance_type
-  env = terraform.workspace
-  opensearch_subnet_ids = var.private_subnet_ids
-  opensearch_version = var.opensearch_version
-  automated_snapshot_start_hour =  var.automated_snapshot_start_hour
-  opensearch_ebs_volume_size    =  var.opensearch_ebs_volume_size
-  opensearch_instance_count     =  var.opensearch_instance_count
-  opensearch_log_types           = ["INDEX_SLOW_LOGS"]
-  create_os_service_role        = var.create_os_service_role
-  multi_az_enabled = var.multi_az_enabled
-  vpc_id = var.vpc_id
-  opensearch_autotune_rollback_type = "NO_ROLLBACK"
-  create_cloudwatch_log_policy = var.create_cloudwatch_log_policy
+  source                        = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/opensearch?ref=v1.19"
+  tags                          = var.tags
+  cluster_tshirt_size           = var.cluster_tshirt_size
+  subnet_ids                    = var.private_subnet_ids
+  engine_version                = var.opensearch_version
+  automated_snapshot_start_hour = var.automated_snapshot_start_hour
+  vpc_id                        = var.vpc_id
+  create_cloudwatch_log_policy  = var.create_cloudwatch_log_policy
+  create_snapshot_role          = var.create_snapshot_role
+  #create_os_service_role        = var.create_os_service_role
+  resource_prefix               = "${var.program}-${terraform.workspace}-${var.project}"
 }
 
 #mysql
