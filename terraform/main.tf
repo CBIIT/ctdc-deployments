103e0c2c11c32b8b225312e0c55b04e2b2fd8ba3
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-11-05 13:50:47 -0500

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,24 +81,24 @@ module "new_relic_metric_pipeline" {
 
 # Opensearch
 module "opensearch" {
-  count                             = var.create_opensearch_cluster ? 1 : 0
-  source                            = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/opensearch?ref=v1.19"
-  stack_name                        = var.project
-  resource_prefix                   = "${var.program}-${terraform.workspace}-${var.project}"
-  tags                              = var.tags
-  opensearch_instance_type          = var.opensearch_instance_type
-  env                               = terraform.workspace
-  opensearch_subnet_ids             = var.private_subnet_ids
-  opensearch_version                = var.opensearch_version
-  automated_snapshot_start_hour     = var.automated_snapshot_start_hour
-  opensearch_ebs_volume_size        = var.opensearch_ebs_volume_size
-  opensearch_instance_count         = var.opensearch_instance_count
-  opensearch_log_types              = ["INDEX_SLOW_LOGS"]
-  create_os_service_role            = var.create_os_service_role
-  multi_az_enabled                  = var.multi_az_enabled
-  vpc_id                            = var.vpc_id
+  count = var.create_opensearch_cluster ? 1: 0
+  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/opensearch?ref=v1.9"
+  resource_prefix     = "${var.stack_name}-${terraform.workspace}"
+  stack_name = var.stack_name
+  tags = var.tags
+  opensearch_instance_type = var.opensearch_instance_type
+  env = terraform.workspace
+  opensearch_subnet_ids = var.private_subnet_ids
+  opensearch_version = var.opensearch_version
+  automated_snapshot_start_hour =  var.automated_snapshot_start_hour
+  opensearch_ebs_volume_size    =  var.opensearch_ebs_volume_size
+  opensearch_instance_count     =  var.opensearch_instance_count
+  opensearch_log_types           = ["INDEX_SLOW_LOGS"]
+  create_os_service_role        = var.create_os_service_role
+  multi_az_enabled = var.multi_az_enabled
+  vpc_id = var.vpc_id
   opensearch_autotune_rollback_type = "NO_ROLLBACK"
-  create_cloudwatch_log_policy      = var.create_cloudwatch_log_policy
+  create_cloudwatch_log_policy = var.create_cloudwatch_log_policy
 }
 
 #mysql
