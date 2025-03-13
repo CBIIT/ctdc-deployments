5b909da3cc5335a581022c0e92db0e98ddba0dda
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2025-03-13 10:12:36 -0400

    updated

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -45,7 +45,7 @@ locals {
       secretKey   = ""
       description = ""
       secretValue = {
-        es_host                       = module.opensearch.endpoint
+        #es_host                       = module.opensearch.endpoint
         sumo_collector_token_frontend = module.monitoring.sumo_source_urls.frontend[0]
         sumo_collector_token_backend  = module.monitoring.sumo_source_urls.backend[0]
       }
