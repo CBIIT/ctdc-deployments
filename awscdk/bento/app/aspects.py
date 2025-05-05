50ce6ec84b957e4580255f044faaf5b9df1f2b85
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2025-05-05 16:17:36 -0400

    updated

--- awscdk/bento/app/aspects.py
+++ awscdk/bento/app/aspects.py
@@ -1,6 +1,6 @@
 import aws_cdk as cdk
 import jsii
-from constructs import Construct, IConstruct
+from constructs import Construct
 from configparser import ConfigParser
 from aws_cdk import aws_iam as iam
 
@@ -12,14 +12,18 @@ class MyAspect:
         config.read('config.ini')
 
         if isinstance(node, iam.CfnRole):
-            if config.has_option('iam', 'role_prefix'):
-                resolvedLogicalId = cdk.Stack.of(node).resolve(node.logical_id)
-                base_role_name = config['iam']['role_prefix'] + '-' + resolvedLogicalId
+            if config.has_option('iam', 'role_prefix') and config.has_option('main', 'resource_prefix'):
+                role_prefix = config['iam']['role_prefix']
+                resource_prefix = config['main']['resource_prefix']
 
-                # IAM role name must be <= 64 characters
+                resolved_logical_id = cdk.Stack.of(node).resolve(node.logical_id)
+
+                base_role_name = f"{role_prefix}-{resource_prefix}-{resolved_logical_id}"
+
+                # IAM role names must be <= 64 characters
                 if len(base_role_name) > 64:
-                    max_logical_id_length = 64 - len(config['iam']['role_prefix']) - 1 
-                    resolvedLogicalId_truncated = resolvedLogicalId[:max_logical_id_length]
-                    base_role_name = config['iam']['role_prefix'] + '-' + resolvedLogicalId_truncated
+                    max_id_length = 64 - len(role_prefix) - len(resource_prefix) - 2  # 2 hyphens
+                    resolved_logical_id = resolved_logical_id[:max_id_length]
+                    base_role_name = f"{role_prefix}-{resource_prefix}-{resolved_logical_id}"
 
                 node.role_name = base_role_name
