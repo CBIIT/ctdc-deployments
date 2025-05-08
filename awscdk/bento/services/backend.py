d940d530355becac8708d565c9a4ef6bad56e72f
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2025-05-08 14:24:07 -0400

    updated

--- awscdk/bento/services/backend.py
+++ awscdk/bento/services/backend.py
@@ -1,243 +1,198 @@
-import boto3
-from configparser import ConfigParser
-from constructs import Construct
-from cdk_ec2_key_pair import KeyPair, PublicKeyFormat
-
-from aws_cdk import Stack, RemovalPolicy, SecretValue, Duration
-from aws_cdk import aws_elasticloadbalancingv2 as elbv2
-from aws_cdk import aws_ec2 as ec2
-from aws_cdk import aws_ecs as ecs
-from aws_cdk import aws_opensearchservice as opensearch
-from aws_cdk import aws_kms as kms
-from aws_cdk import aws_secretsmanager as secretsmanager
-from aws_cdk import aws_certificatemanager as cfm
-from aws_cdk import aws_rds as rds
-from aws_cdk import aws_cloudfront as cloudfront
-from aws_cdk import aws_cloudfront_origins as origins
-from aws_cdk import aws_s3 as s3
-from aws_cdk import aws_ssm as ssm
+from aws_cdk import Duration
 from aws_cdk import aws_iam as iam
+from aws_cdk import aws_elasticloadbalancingv2 as elbv2
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_ecr as ecr
+from aws_cdk import aws_secretsmanager as secretsmanager
 
-from services import frontend, files, authn, interoperation
-from services.backend import createService as createBackendService
-#from services import frontend, authn, backend
+class backendService:
+  def createService(self, config):
+  #def createService(self, config, nlb):
 
-class Stack(Stack):
-    def __init__(self, scope: Construct, **kwargs) -> None:
-        super().__init__(scope, **kwargs)
+    ### Backend Service ###############################################################################################################
+    service = "backend"
 
-        # Read config
-        config = ConfigParser()
-        config.read('config.ini')
-        
-        self.namingPrefix = "{}-{}".format(config['main']['resource_prefix'], config['main']['tier'])
+    # Set container configs
+    # if config.has_option(service, 'command'):
+    #     command = [config[service]['command']]
+    # else:
+    #     command = None
 
-        if config.has_option('main', 'subdomain'):
-            self.app_url = "https://{}.{}".format(config['main']['subdomain'], config['main']['domain'])
-        else:
-            self.app_url = "https://{}".format(config['main']['domain'])
-        
-        # Import VPC
-        self.VPC = ec2.Vpc.from_lookup(self, "VPC",
-            vpc_id=config['main']['vpc_id']
-        )
+    environment={
+            "JAVA_OPTS": "-javaagent:/usr/local/tomcat/newrelic/newrelic.jar",
+            "AUTH_ENABLED":"false",
+            "AUTH_ENDPOINT":"/api/auth/",
+            "BENTO_API_VERSION":config[service]['image'],
+            "ES_FILTER_ENABLED":"true",
+            "PROJECT":"ctdc",
+            "ES_SCHEMA":"es-schema-ctdc.graphql",
+            "MYSQL_SESSION_ENABLED":"true",
+            #"MEMGRAPH_URL": "bolt://{}:7687".format(nlb.load_balancer_dns_name),
+            "REDIS_ENABLE":"false",
+            "REDIS_FILTER_ENABLE":"false",
+            "REDIS_HOST":"localhost",
+            "REDIS_PORT":"6379",
+            "REDIS_USE_CLUSTER":"true",
+            "NEW_RELIC_HOST": "gov-collector.newrelic.com",
+            "NEW_RELIC_APP_NAME": "{}-{}-backend".format(config['main']['project'], config['main']['tier']),
+            "NEW_RELIC_DISTRIBUTED_TRACING_ENABLED": "true",
+            "NEW_RELIC_LOG_FILE_NAME":"STDOUT",
+            "NEW_RELIC_LABELS":"Project:{};Environment:{}".format(config['main']['project'], config['main']['tier']),
+            "JAVA_OPTS":"-javaagent:/usr/local/tomcat/newrelic/newrelic.jar",
+        }
 
-        # ECS Cluster
-        self.kmsKey = kms.Key(self, "ECSExecKey")
-
-        self.ECSCluster = ecs.Cluster(self,
-            "ecs",
-            vpc=self.VPC,
-            execute_command_configuration=ecs.ExecuteCommandConfiguration(
-                kms_key=self.kmsKey
-            ),
-        )
-
-                        # Backend Service (moved early to fetch SG for OpenSearch)
-        backend_service = createBackendService(self, config)
-        backend_service_sg = getattr(getattr(backend_service, 'service', None), 'connections', None)
-
-        # Create OpenSearch SG to allow HTTPS from backend service and whitelisted IPs
-        OpenSearchSG = ec2.SecurityGroup(self, "OpenSearchSG",
-            vpc=self.VPC,
-            description="Allow HTTPS access from backend service and whitelisted EC2 IPs",
-            allow_all_outbound=True
-        )
-
-        if backend_service_sg and backend_service_sg.security_groups:
-            OpenSearchSG.add_ingress_rule(
-                peer=backend_service.service.connections.security_groups[0],
-                connection=ec2.Port.tcp(443),
-                description="Allow HTTPS from Backend Service"
-            )
-
-
-            ips = [ip.strip() for ip in config['main']['ec2_whitelist_ips'].split(',')]
-            for ip in ips:
-                OpenSearchSG.add_ingress_rule(
-                    peer=ec2.Peer.ipv4(f"{ip}/32"),
-                    connection=ec2.Port.tcp(443),
-                    description=f"Allow HTTPS from whitelisted IP {ip}"
-                )
-
-        if config['os']['endpoint_type'] == 'vpc':
-            vpc = self.VPC
-            vpc_subnets = [{'subnets': [self.VPC.private_subnets[0]]}]
-        else:
-            vpc = None
-            vpc_subnets = [{}]
-
-        self.osDomain = opensearch.Domain(self,
-            "opensearch",
-            version=opensearch.EngineVersion.open_search(config['os']['version']),
-            vpc=vpc,
-            zone_awareness=opensearch.ZoneAwarenessConfig(enabled=False),
-            capacity=opensearch.CapacityConfig(
-                data_node_instance_type=config['os']['data_node_instance_type'],
-                multi_az_with_standby_enabled=False
-            ),
-            vpc_subnets=vpc_subnets,
-            security_groups=[OpenSearchSG],
-            removal_policy=RemovalPolicy.DESTROY,
-        )
-        # Policy to allow access for dataloader instances
-        os_policy = iam.PolicyStatement(
-            actions=[
-                "es:ESHttpGet",
-                "es:ESHttpPut",
-                "es:ESHttpPost",
-                "es:ESHttpPatch",
-                "es:ESHttpHead",
-                "es:ESHttpGet",
-                "es:ESHttpDelete",
-            ],
-            resources=["{}/*".format(self.osDomain.domain_arn)],
-            principals=[iam.AnyPrincipal()],
-        )
-        self.osDomain.add_access_policies(os_policy)
-
-        # Cloudfront
-        self.cfOrigin = s3.Bucket.from_bucket_name(self, "CFBucket",
-            bucket_name=config['s3']['file_manifest_bucket_name']
-        )
-
-        self.cfKeys = KeyPair(self, "CFKeyPair",
-            key_pair_name="CF-key-{}-{}".format(config['main']['resource_prefix'], config['main']['tier']),
-            expose_public_key=True,
-            public_key_format=PublicKeyFormat.PEM
-        )
-
-        CFPublicKey = cloudfront.PublicKey(self, "CFPublicKey-{}".format(config['main']['tier']),
-            encoded_key=self.cfKeys.public_key_value
-        )
-        CFKeyGroup = cloudfront.KeyGroup(self, "CFKeyGroup-{}".format(config['main']['tier']),
-            items=[CFPublicKey]
-        )
-        
-        self.cfDistribution = cloudfront.Distribution(self, "CFDistro",
-            default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3Origin(self.cfOrigin),
-                allowed_methods=cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-                trusted_key_groups=[CFKeyGroup]
-            )
-        )
-        
-        # # RDS
-        self.auroraCluster = rds.DatabaseCluster(self, "Aurora",
-            engine=rds.DatabaseClusterEngine.aurora_mysql(version=rds.AuroraMysqlEngineVersion.VER_3_05_2),
-            writer=rds.ClusterInstance.provisioned("writer",
-            ),
-            vpc=vpc,
-            storage_encrypted=True,
-            default_database_name=config['db']['mysql_database']
-        )
-
-        # Secrets
-        self.secret = secretsmanager.Secret(self, "Secret",
-            secret_name="{}/{}/{}".format(config['main']['secret_prefix'], config['main']['tier'], "ctdc"),
-            secret_object_value={
-                "neo4j_user": SecretValue.unsafe_plain_text(config['db']['neo4j_user']),
-                "neo4j_password": SecretValue.unsafe_plain_text(config['db']['neo4j_password']),
-                "file_manifest_bucket_name": SecretValue.unsafe_plain_text(config['s3']['file_manifest_bucket_name']),
-                "es_host": SecretValue.unsafe_plain_text(self.osDomain.domain_endpoint),
-                "cf_key_pair_id": SecretValue.unsafe_plain_text(CFPublicKey.public_key_id),
-                "cf_url": SecretValue.unsafe_plain_text("https://{}".format(self.cfDistribution.distribution_domain_name)),
-                "cookie_secret": SecretValue.unsafe_plain_text(config['secrets']['cookie_secret']),
-                "dcf_client_id": SecretValue.unsafe_plain_text(config['secrets']['dcf_client_id']),
-                "dcf_client_secret": SecretValue.unsafe_plain_text(config['secrets']['dcf_client_secret']),
-                "dcf_base_url": SecretValue.unsafe_plain_text(config['secrets']['dcf_base_url']),
-                "dcf_redirect_url": SecretValue.unsafe_plain_text(config['secrets']['dcf_redirect_url']),
-                "dcf_userinfo_url": SecretValue.unsafe_plain_text(config['secrets']['dcf_userinfo_url']),
-                "dcf_authorize_url": SecretValue.unsafe_plain_text(config['secrets']['dcf_authorize_url']),
-                "dcf_token_url": SecretValue.unsafe_plain_text(config['secrets']['dcf_token_url']),
-                "dcf_logout_url": SecretValue.unsafe_plain_text(config['secrets']['dcf_logout_url']),
-                "dcf_scope": SecretValue.unsafe_plain_text(config['secrets']['dcf_scope']),
-                "dcf_prompt": SecretValue.unsafe_plain_text(config['secrets']['dcf_prompt']),
-                "dcf_file_url": SecretValue.unsafe_plain_text(config['secrets']['dcf_file_url']),
-                "google_id": SecretValue.unsafe_plain_text(config['secrets']['google_id']),
-                "s3_access_key_id": SecretValue.unsafe_plain_text(config['secrets']['s3_access_key_id']),
-                "s3_secret_access_key": SecretValue.unsafe_plain_text(config['secrets']['s3_secret_access_key']),
-                "react_app_data_model_navigator": SecretValue.unsafe_plain_text(config['secrets']['react_app_data_model_navigator']),
-            }
-        )
-
-        # ALB
-        if config.getboolean('alb', 'internet_facing'):
-            subnets = ec2.SubnetSelection(
-                subnets=vpc.select_subnets(one_per_az=True, subnet_type=ec2.SubnetType.PUBLIC).subnets
-            )
-        else:
-            subnets = ec2.SubnetSelection(
-                subnets=vpc.select_subnets(one_per_az=True, subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS).subnets
-            )
+    secrets={
+            "NEW_RELIC_LICENSE_KEY":ecs.Secret.from_secrets_manager(secretsmanager.Secret.from_secret_name_v2(self, "be_newrelic", secret_name='monitoring/newrelic'), 'api_key'),
+            #"NEO4J_PASSWORD":ecs.Secret.from_secrets_manager(self.secret, 'neo4j_password'),
+            #"NEO4J_USER":ecs.Secret.from_secrets_manager(self.secret, 'neo4j_user'),
+            "ES_HOST":ecs.Secret.from_secrets_manager(self.secret, 'es_host'),
+            "MEMGRAPH_USER":ecs.Secret.from_secrets_manager(self.secret, 'db_user'),
+            "MEMGRAPH_PASSWORD":ecs.Secret.from_secrets_manager(self.secret, 'db_pass')
+        }
     
-        self.ALB = elbv2.ApplicationLoadBalancer(self,
-            "alb",
-            vpc=self.VPC,
-            internet_facing=config.getboolean('alb', 'internet_facing'),
-            vpc_subnets=subnets
+    taskDefinition = ecs.FargateTaskDefinition(self,
+        "{}-{}-taskDef".format(self.namingPrefix, service),
+        cpu=config.getint(service, 'taskcpu'),
+        memory_limit_mib=config.getint(service, 'taskmemory')
+    )
+
+    # Grant ECR access
+    taskDefinition.add_to_execution_role_policy(
+            iam.PolicyStatement(
+                actions=[
+                    "ecr:UploadLayerPart",
+                    "ecr:PutImage",
+                    "ecr:ListTagsForResource",
+                    "ecr:InitiateLayerUpload",
+                    "ecr:GetRepositoryPolicy",
+                    "ecr:GetLifecyclePolicy",
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:DescribeRepositories",
+                    "ecr:CompleteLayerUpload",
+                    "ecr:BatchGetImage",
+                    "ecr:BatchCheckLayerAvailability"
+                ],
+                effect=iam.Effect.ALLOW,
+                resources=["arn:aws:ecr:us-east-1:986019062625:repository/*"]
+            )
         )
 
-        self.ALB.add_redirect(
-            source_protocol=elbv2.ApplicationProtocol.HTTP,
-            source_port=80,
-            target_protocol=elbv2.ApplicationProtocol.HTTPS,
-            target_port=443
+    taskDefinition.add_to_execution_role_policy(
+            iam.PolicyStatement(
+                actions=["ecr:GetAuthorizationToken"],
+                effect=iam.Effect.ALLOW,
+                resources=["*"]
+            )
         )
+    
+    ecr_repo = ecr.Repository.from_repository_arn(self, "{}_repo".format(service), repository_arn=config[service]['repo'])
 
-        client = boto3.client('acm')
-        response = client.list_certificates(CertificateStatuses=['ISSUED'])
-
-        for cert in response["CertificateSummaryList"]:
-            if ('*.{}'.format(config['main']['domain']) in cert.values()):
-                certARN = cert['CertificateArn']
-
-        alb_cert = cfm.Certificate.from_certificate_arn(self, "alb-cert",
-            certificate_arn=certARN)
-        
-        self.listener = self.ALB.add_listener("PublicListener",
-            certificates=[alb_cert],
-            port=443
+    # Backend Container
+    backend_container = taskDefinition.add_container(
+        service,
+        #image=ecs.ContainerImage.from_registry("{}:{}".format(config[service]['repo'], config[service]['image'])),
+        image=ecs.ContainerImage.from_ecr_repository(repository=ecr_repo, tag=config[service]['image']),
+        cpu=config.getint(service, 'cpu'),
+        memory_limit_mib=config.getint(service, 'memory'),
+        port_mappings=[ecs.PortMapping(app_protocol=ecs.AppProtocol.http, container_port=config.getint(service, 'port'), name=service)],
+        #entry_point=command,
+        entry_point=["sh", "-c"],
+        command=[
+            "wget https://download.newrelic.com/newrelic/java-agent/newrelic-agent/current/newrelic-java.zip -O newrelic-java.zip && "
+            "rm -rf newrelic && unzip -o newrelic-java.zip && bin/catalina.sh run"
+        ],
+        environment=environment,
+        secrets=secrets,
+        logging=ecs.LogDrivers.aws_logs(
+            stream_prefix="{}-{}".format(self.namingPrefix, service)
         )
+    )
 
-        self.listener.add_action("ECS-Content-Not-Found",
-            action=elbv2.ListenerAction.fixed_response(200,
-                message_body="The requested resource is not available")
-        )
+    # # For Sumo Logs use
+    
+    # backend_container = taskDefinition.add_container(
+    #     service,
+    #     image=ecs.ContainerImage.from_ecr_repository(repository=ecr_repo, tag=config[service]['image']),
+    #     cpu=config.getint(service, 'cpu'),
+    #     memory_limit_mib=config.getint(service, 'memory'),
+    #     port_mappings=[ecs.PortMapping(container_port=config.getint(service, 'port'), name=service)],
+    #     command=command,
+    #     environment=environment,
+    #     secrets=secrets,
+    #     logging=ecs.LogDrivers.firelens(
+    #         options={
+    #             "Name": "http",
+    #             "Host": config['sumologic']['collector_endpoint'],
+    #             "URI": "/receiver/v1/http/{}".format(config['sumologic']['collector_token']),
+    #             "Port": "443",
+    #             "tls": "on",
+    #             "tls.verify": "off",
+    #             "Retry_Limit": "2",
+    #             "Format": "json_lines"
+    #         }
+    #     )
+    # )
 
-        ### Fargate
-        # Frontend Service
-        frontend.frontendService.createService(self, config)
 
-        
-        # AuthN Service
-        authn.authnService.createService(self, config)
+    # # Sumo Logic FireLens Log Router Container
+    # sumo_logic_container = taskDefinition.add_firelens_log_router(
+    #     "sumologic-firelens",
+    #     image=ecs.ContainerImage.from_registry("public.ecr.aws/aws-observability/aws-for-fluent-bit:stable"),
+    #     firelens_config=ecs.FirelensConfig(
+    #         type=ecs.FirelensLogRouterType.FLUENTBIT,
+    #         options=ecs.FirelensOptions(
+    #             enable_ecs_log_metadata=True
+    #         )
+    #     ),
+    # essential=True
+    # )
 
-        # AuthZ Service
-        #authz.authzService.createService(self, config)
+    # New Relic Container
+    new_relic_container = taskDefinition.add_container(
+        "newrelic-infra",
+        image=ecs.ContainerImage.from_registry("newrelic/nri-ecs:1.9.2"),
+        cpu=0,
+        essential=True,
+        secrets={"NRIA_LICENSE_KEY":ecs.Secret.from_secrets_manager(secretsmanager.Secret.from_secret_name_v2(self, "benr_newrelic", secret_name='monitoring/newrelic'), 'api_key'),},
+        environment={
+            "NEW_RELIC_HOST":"gov-collector.newrelic.com",
+            "NEW_RELIC_APP_NAME":"{}-{}-files".format(config['main']['project'], config['main']['tier']),
+            "NRIA_IS_FORWARD_ONLY":"true",
+            "NEW_RELIC_DISTRIBUTED_TRACING_ENABLED":"true",
+            "NRIA_PASSTHROUGH_ENVIRONMENT":"ECS_CONTAINER_METADATA_URI,ECS_CONTAINER_METADATA_URI_V4,FARGATE",
+            "FARGATE":"true",
+            "NRIA_CUSTOM_ATTRIBUTES": '{"nrDeployMethod":"downloadPage"}',
+            "NRIA_OVERRIDE_HOST_ROOT": ""
+            },
+    )
 
-        # Files Service
-        files.filesService.createService(self, config)
 
-        # Interoperation Service
-        interoperation.interoperationService.createService(self, config)
+    ecsService = ecs.FargateService(self,
+        "{}-{}-service".format(self.namingPrefix, service),
+        cluster=self.ECSCluster,
+        task_definition=taskDefinition,
+        enable_execute_command=True,
+        min_healthy_percent=50,
+        max_healthy_percent=200,
+        circuit_breaker=ecs.DeploymentCircuitBreaker(
+            enable=True,
+            rollback=True
+        ),
+    )
+
+    ecsTarget = self.listener.add_targets("ECS-{}-Target".format(service),
+        port=int(config[service]['port']),
+        protocol=elbv2.ApplicationProtocol.HTTP,
+        health_check = elbv2.HealthCheck(
+            path=config[service]['health_check_path'],
+            timeout=Duration.seconds(config.getint(service, 'health_check_timeout')),
+            interval=Duration.seconds(config.getint(service, 'health_check_interval')),),
+        targets=[ecsService],)
+
+    elbv2.ApplicationListenerRule(self, id="alb-{}-rule".format(service),
+        conditions=[
+            elbv2.ListenerCondition.path_patterns(config[service]['path'].split(','))
+        ],
+        priority=int(config[service]['priority_rule_number']),
+        listener=self.listener,
+        target_groups=[ecsTarget])
