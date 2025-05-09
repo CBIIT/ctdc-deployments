e496c6ea65427f24180d11a123c400c07ad48063
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2025-05-08 22:20:10 -0400

    updated

--- awscdk/bento/app/stack.py
+++ awscdk/bento/app/stack.py
@@ -3,21 +3,19 @@ from configparser import ConfigParser
 from constructs import Construct
 from cdk_ec2_key_pair import KeyPair, PublicKeyFormat
 
-from aws_cdk import (
-    Stack, RemovalPolicy, SecretValue, Duration,
-    aws_elasticloadbalancingv2 as elbv2,
-    aws_ec2 as ec2,
-    aws_ecs as ecs,
-    aws_opensearchservice as opensearch,
-    aws_kms as kms,
-    aws_secretsmanager as secretsmanager,
-    aws_certificatemanager as cfm,
-    aws_rds as rds,
-    aws_cloudfront as cloudfront,
-    aws_cloudfront_origins as origins,
-    aws_s3 as s3,
-    aws_iam as iam,
-)
+from aws_cdk import Stack, RemovalPolicy, SecretValue, Duration
+from aws_cdk import aws_elasticloadbalancingv2 as elbv2
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_opensearchservice as opensearch
+from aws_cdk import aws_kms as kms
+from aws_cdk import aws_secretsmanager as secretsmanager
+from aws_cdk import aws_certificatemanager as cfm
+from aws_cdk import aws_rds as rds
+from aws_cdk import aws_cloudfront as cloudfront
+from aws_cdk import aws_cloudfront_origins as origins
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_iam as iam
 
 from services import frontend, backend, files, authn, interoperation
 
@@ -27,74 +25,113 @@ class Stack(Stack):
 
         config = ConfigParser()
         config.read('config.ini')
+        
+        self.namingPrefix = "{}-{}".format(config['main']['resource_prefix'], config['main']['tier'])
 
-        self.namingPrefix = f"{config['main']['resource_prefix']}-{config['main']['tier']}"
-        self.VPC = ec2.Vpc.from_lookup(self, "VPC", vpc_id=config['main']['vpc_id'])
+        self.app_url = f"https://{config['main'].get('subdomain') + '.' if config['main'].get('subdomain') else ''}{config['main']['domain']}"
 
-        if config.has_option('main', 'subdomain'):
-            self.app_url = f"https://{config['main']['subdomain']}.{config['main']['domain']}"
-        else:
-            self.app_url = f"https://{config['main']['domain']}"
-
-        # ECS Cluster
-        self.kmsKey = kms.Key(self, "ECSExecKey")
-        self.ECSCluster = ecs.Cluster(self, "ecs", vpc=self.VPC,
-            execute_command_configuration=ecs.ExecuteCommandConfiguration(kms_key=self.kmsKey)
+        self.VPC = ec2.Vpc.from_lookup(self, "VPC",
+            vpc_id=config['main']['vpc_id']
         )
 
-        # ALB and Listener
-        subnet_type = ec2.SubnetType.PUBLIC if config.getboolean('alb', 'internet_facing') else ec2.SubnetType.PRIVATE_WITH_EGRESS
-        subnets = ec2.SubnetSelection(
-            subnets=self.VPC.select_subnets(one_per_az=True, subnet_type=subnet_type).subnets
-        )
-
-        self.ALB = elbv2.ApplicationLoadBalancer(self, "alb",
+        # Security Groups
+        self.albSG = ec2.SecurityGroup(self, "ALBSG",
             vpc=self.VPC,
-            internet_facing=config.getboolean('alb', 'internet_facing'),
-            vpc_subnets=subnets
+            allow_all_outbound=True,
+            description="Allow HTTP/HTTPS traffic"
         )
 
-        self.ALB.add_redirect(
-            source_protocol=elbv2.ApplicationProtocol.HTTP,
-            source_port=80,
-            target_protocol=elbv2.ApplicationProtocol.HTTPS,
-            target_port=443
+        self.backendSG = ec2.SecurityGroup(self, "BackendSG",
+            vpc=self.VPC,
+            allow_all_outbound=True,
+            description="Allow ALB to talk to backend on port 8080"
         )
 
-        client = boto3.client('acm')
-        response = client.list_certificates(CertificateStatuses=['ISSUED'])
-        for cert in response["CertificateSummaryList"]:
-            if f"*.{config['main']['domain']}" in cert.values():
-                certARN = cert['CertificateArn']
-
-        alb_cert = cfm.Certificate.from_certificate_arn(self, "alb-cert", certificate_arn=certARN)
-
-        self.listener = self.ALB.add_listener("PublicListener",
-            certificates=[alb_cert],
-            port=443
+        self.backendSG.add_ingress_rule(
+            peer=self.albSG,
+            connection=ec2.Port.tcp(8080),
+            description="Allow ALB to reach backend"
         )
 
-        # CloudFront setup
+        self.osSG = ec2.SecurityGroup(self, "OSSG",
+            vpc=self.VPC,
+            allow_all_outbound=True,
+            description="Allow backend and IP whitelist to access OpenSearch"
+        )
+
+        self.osSG.add_ingress_rule(
+            peer=self.backendSG,
+            connection=ec2.Port.tcp(443),
+            description="Allow backend SG to access OpenSearch"
+        )
+
+        whitelist_ips = [ip.strip() for ip in config['main']['ec2_whitelist_ips'].split(",")]
+        for ip in whitelist_ips:
+            self.osSG.add_ingress_rule(
+                peer=ec2.Peer.ipv4(ip),
+                connection=ec2.Port.tcp(443),
+                description=f"Allow whitelisted IP {ip} to access OpenSearch"
+            )
+
+        # OpenSearch
+        self.osDomain = opensearch.Domain(self,
+            "opensearch",
+            version=opensearch.EngineVersion.open_search(config['os']['version']),
+            vpc=self.VPC,
+            security_groups=[self.osSG],
+            zone_awareness=opensearch.ZoneAwarenessConfig(enabled=False),
+            capacity=opensearch.CapacityConfig(
+                data_node_instance_type=config['os']['data_node_instance_type'],
+                multi_az_with_standby_enabled=False
+            ),
+            vpc_subnets=[{"subnet_type": ec2.SubnetType.PRIVATE_WITH_EGRESS}],
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        # Allow access via policy (optional if SG is enough)
+        os_policy = iam.PolicyStatement(
+            actions=[
+                "es:ESHttpGet", "es:ESHttpPut", "es:ESHttpPost",
+                "es:ESHttpPatch", "es:ESHttpHead", "es:ESHttpDelete",
+            ],
+            resources=["{}/*".format(self.osDomain.domain_arn)],
+            principals=[iam.AnyPrincipal()],
+        )
+        self.osDomain.add_access_policies(os_policy)
+
+        # CloudFront
         self.cfOrigin = s3.Bucket.from_bucket_name(self, "CFBucket",
             bucket_name=config['s3']['file_manifest_bucket_name']
         )
+
         self.cfKeys = KeyPair(self, "CFKeyPair",
-            key_pair_name=f"CF-key-{config['main']['resource_prefix']}-{config['main']['tier']}",
+            key_pair_name="CF-key-{}-{}".format(config['main']['resource_prefix'], config['main']['tier']),
             expose_public_key=True,
             public_key_format=PublicKeyFormat.PEM
         )
-        CFPublicKey = cloudfront.PublicKey(self, f"CFPublicKey-{config['main']['tier']}",
+
+        CFPublicKey = cloudfront.PublicKey(self, "CFPublicKey-{}".format(config['main']['tier']),
             encoded_key=self.cfKeys.public_key_value
         )
-        CFKeyGroup = cloudfront.KeyGroup(self, f"CFKeyGroup-{config['main']['tier']}",
+        CFKeyGroup = cloudfront.KeyGroup(self, "CFKeyGroup-{}".format(config['main']['tier']),
             items=[CFPublicKey]
         )
+
         self.cfDistribution = cloudfront.Distribution(self, "CFDistro",
             default_behavior=cloudfront.BehaviorOptions(
                 origin=origins.S3Origin(self.cfOrigin),
                 allowed_methods=cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
                 trusted_key_groups=[CFKeyGroup]
             )
+        )
+
+        # RDS
+        self.auroraCluster = rds.DatabaseCluster(self, "Aurora",
+            engine=rds.DatabaseClusterEngine.aurora_mysql(version=rds.AuroraMysqlEngineVersion.VER_3_05_2),
+            writer=rds.ClusterInstance.provisioned("writer"),
+            vpc=self.VPC,
+            storage_encrypted=True,
+            default_database_name=config['db']['mysql_database']
         )
 
         # Secrets
@@ -104,8 +141,9 @@ class Stack(Stack):
                 "neo4j_user": SecretValue.unsafe_plain_text(config['db']['neo4j_user']),
                 "neo4j_password": SecretValue.unsafe_plain_text(config['db']['neo4j_password']),
                 "file_manifest_bucket_name": SecretValue.unsafe_plain_text(config['s3']['file_manifest_bucket_name']),
+                "es_host": SecretValue.unsafe_plain_text(self.osDomain.domain_endpoint),
                 "cf_key_pair_id": SecretValue.unsafe_plain_text(CFPublicKey.public_key_id),
-                "cf_url": SecretValue.unsafe_plain_text("https://{}".format(self.cfDistribution.distribution_domain_name)),
+                "cf_url": SecretValue.unsafe_plain_text(f"https://{self.cfDistribution.distribution_domain_name}"),
                 "cookie_secret": SecretValue.unsafe_plain_text(config['secrets']['cookie_secret']),
                 "dcf_client_id": SecretValue.unsafe_plain_text(config['secrets']['dcf_client_id']),
                 "dcf_client_secret": SecretValue.unsafe_plain_text(config['secrets']['dcf_client_secret']),
@@ -125,54 +163,65 @@ class Stack(Stack):
             }
         )
 
-        # Aurora Cluster
-        self.auroraCluster = rds.DatabaseCluster(self, "Aurora",
-            engine=rds.DatabaseClusterEngine.aurora_mysql(version=rds.AuroraMysqlEngineVersion.VER_3_05_2),
-            writer=rds.ClusterInstance.provisioned("writer"),
+        # ALB
+        if config.getboolean('alb', 'internet_facing'):
+            subnets = ec2.SubnetSelection(
+                subnets=self.VPC.select_subnets(one_per_az=True, subnet_type=ec2.SubnetType.PUBLIC).subnets
+            )
+        else:
+            subnets = ec2.SubnetSelection(
+                subnets=self.VPC.select_subnets(one_per_az=True, subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS).subnets
+            )
+
+        self.ALB = elbv2.ApplicationLoadBalancer(self,
+            "alb",
             vpc=self.VPC,
-            storage_encrypted=True,
-            default_database_name=config['db']['mysql_database']
+            internet_facing=config.getboolean('alb', 'internet_facing'),
+            vpc_subnets=subnets,
+            security_group=self.albSG
         )
 
-        # Services
+        self.ALB.add_redirect(
+            source_protocol=elbv2.ApplicationProtocol.HTTP,
+            source_port=80,
+            target_protocol=elbv2.ApplicationProtocol.HTTPS,
+            target_port=443
+        )
+
+        client = boto3.client('acm')
+        response = client.list_certificates(CertificateStatuses=['ISSUED'])
+
+        for cert in response["CertificateSummaryList"]:
+            if ('*.{}'.format(config['main']['domain']) in cert.values()):
+                certARN = cert['CertificateArn']
+
+        alb_cert = cfm.Certificate.from_certificate_arn(self, "alb-cert",
+            certificate_arn=certARN)
+
+        self.listener = self.ALB.add_listener("PublicListener",
+            certificates=[alb_cert],
+            port=443
+        )
+
+        self.listener.add_action("ECS-Content-Not-Found",
+            action=elbv2.ListenerAction.fixed_response(200,
+                message_body="The requested resource is not available")
+        )
+
+        # ECS Cluster
+        self.kmsKey = kms.Key(self, "ECSExecKey")
+
+        self.ECSCluster = ecs.Cluster(self,
+            "ecs",
+            vpc=self.VPC,
+            execute_command_configuration=ecs.ExecuteCommandConfiguration(
+                kms_key=self.kmsKey
+            ),
+        )
+
+        # ECS Services
         frontend.frontendService.createService(self, config)
-        backend_sg = backend.backendService.createService(self, config, self.secret)
+        backend.backendService.createService(self, config, self.backendSG)
         authn.authnService.createService(self, config)
         files.filesService.createService(self, config)
         interoperation.interoperationService.createService(self, config)
-
-        # --- OpenSearch Setup ---
-       # Create and configure security group after backend service is initialized
-        self.osSG = ec2.SecurityGroup(self, "OpenSearchSG",
-            vpc=self.VPC,
-            description="Allow access from backend and whitelisted IPs",
-            allow_all_outbound=True
-        )
-        self.osSG.add_ingress_rule(peer=backend_sg, connection=ec2.Port.tcp(443))
-
-        whitelist_ips = config['main']['ec2_whitelist_ips'].split(',')
-        for ip in whitelist_ips:
-            self.osSG.add_ingress_rule(peer=ec2.Peer.ipv4(ip.strip()), connection=ec2.Port.tcp(443))
-
-        vpc_subnets = [{'subnets': [self.VPC.private_subnets[0]]}] if config['os']['endpoint_type'] == 'vpc' else [{}]
-
-        self.osDomain = opensearch.Domain(self, "opensearch",
-            version=opensearch.EngineVersion.open_search(config['os']['version']),
-            vpc=self.VPC,
-            security_groups=[self.osSG],
-            zone_awareness=opensearch.ZoneAwarenessConfig(enabled=False),
-            capacity=opensearch.CapacityConfig(
-                data_node_instance_type=config['os']['data_node_instance_type'],
-                multi_az_with_standby_enabled=False
-            ),
-            vpc_subnets=vpc_subnets,
-            removal_policy=RemovalPolicy.DESTROY,
-        )
-
-        # Add es_host to secret now that OpenSearch is created
-        secretsmanager.Secret(self, "OpenSearchSecret",
-            secret_name=f"{config['main']['secret_prefix']}/{config['main']['tier']}/opensearch",
-            secret_object_value={
-                "es_host": SecretValue.unsafe_plain_text(self.osDomain.domain_endpoint)
-            }
-        )
